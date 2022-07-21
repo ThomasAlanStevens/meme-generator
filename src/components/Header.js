0a1f58209e925eedd2a3596978eb8d16e7f41b78
thomas <thomaalanstevens@gmail.com>
2022-07-21 06:57:42 -0700

    intial commit, created header react component and some basic css styling.

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <header>
+            <img className="headerIcon" src="trollFace.png"></img>
+            <h1 className="headerTitle">Meme Generator</h1>
+            <h3 className="headerSubTitle">React Project</h3>
+        </header>
+    )
+}
