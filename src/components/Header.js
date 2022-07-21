1e91fd05b16f6bedd44dc727814ae0da195cbeb8
thomas <thomaalanstevens@gmail.com>
2022-07-21 07:03:09 -0700

    completed basic css styling on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Header(){
     return(
-        <header>
+        <header className="flex-row">
             <img className="headerIcon" src="trollFace.png"></img>
             <h1 className="headerTitle">Meme Generator</h1>
             <h3 className="headerSubTitle">React Project</h3>
